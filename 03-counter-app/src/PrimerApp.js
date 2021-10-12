2c88994403e3fb39f7065bc723dceaeabd0dc502
ingJcbDev <ing.jcb.dev@gmail.com>
2021-10-11 21:58:38 -0500

    35. Nuestro primer Componente

---
+++ 03-counter-app/src/PrimerApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//imr tab importa react
+
+// hay dos tipos de compones basados en clases y basados en funciones
+// functional component
+
+//FC
+const PrimeraApp = () => {
+    return <h1>Hola Mundo</h1>;
+}
+
+export default PrimeraApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'; // importacion de react
 import ReactDOM  from 'react-dom'; // Importacion del react dom
+import PrimeraApp from './PrimerApp';
+import './index.css';
 
 const saludo = <h1>Hola mundo</h1>;
 
 // Referencia  
 const divRoot = document.querySelector('#app');
 // renderizar 
-ReactDOM.render(saludo, divRoot);
+ReactDOM.render(<PrimeraApp /> , divRoot);
